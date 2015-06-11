cc9851770a7f9dce1451a547827e16027f86d086
Tenchael <tzz1002@gmail.com>
2015-06-11 21:31:44 +0800

    load config from network

--- 07_dist-dubbo/dubbo-consumer/src/main/java/com/tenchael/dubbo/consumer/Consumer.java
+++ 07_dist-dubbo/dubbo-consumer/src/main/java/com/tenchael/dubbo/consumer/Consumer.java
@@ -5,9 +5,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.tenchael.dubbo.provider.DemoService;
 
 public class Consumer {
+
+	// load context config file from local project
+	public static final String contextLocation = "consumer-context.xml";
+
+	// load context file from network
+	// public static final String contextLocation = "http://192.168.1.60/cfg-files/consumer-context.xml";
+
 	public static void main(String[] args) throws Exception {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				new String[] { "consumer-context.xml" });
+				new String[] { contextLocation });
 		context.start();
 		DemoService demoService = (DemoService) context.getBean("demoService");
 		String response = demoService.echo("helooooooo");
